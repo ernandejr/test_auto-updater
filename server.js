c45944a1d11fa06011d44ae234c2f6d23de586c4
Ernande Junior <ernande@locadados.com>
2017-08-03 16:25:28 -0300

    Testando auto-updater

--- server.js
+++ server.js
@@ -10,15 +10,7 @@ var autoupdater = new AutoUpdater({
  progressDebounce: 0,
  devmode: false
 });
-autoupdater.on('check.out-dated', function(v_old, v) {
-  	console.warn("Your version is outdated. " + v_old + " of " + v);
-  	autoupdater.fire('download-update'); 
-	autoupdater.on('update.downloaded', function() {
-	  console.log("Update downloaded and ready for install");
-	  autoupdater.fire('extract');  
-	});
-});
-autoupdater.fire('check');
+autoupdater.fire('download-update'); 
     res.writeHead(200, {"Content-Type": "text/xml"});
     var hello = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><main>Testando update</main>';
     res.end(hello);
