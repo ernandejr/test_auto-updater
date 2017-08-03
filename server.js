15693b912bd4cf31eb8a2d0b217e56cdf446ea12
Ernande Junior <ernande@locadados.com>
2017-08-03 17:45:24 -0300

    Testando atualizacao automatica

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ var autoupdater = new AutoUpdater({
 	});
 	autoupdater.fire('check');
     res.writeHead(200, {"Content-Type": "text/xml"});
-    var hello = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><main>Hello Word</main>';
+    var hello = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><main>Versão atualizada</main>';
     res.end(hello);
 }).listen(3000, 'localhost', function(){
 	console.log("Servidor rodando na porta 3000");
