b76b42be55daf4e5098a4d8d872fc96be89624b6
Ernande Junior <ernande@locadados.com>
2017-08-04 09:02:24 -0300

    Configuracao pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'Auto-updater',
+      script    : 'server.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  {
+  "watch": ["server", "client"],
+  "ignore_watch" : ["*.zip"],
+  "watch_options": {
+    "followSymlinks": false
+  }
+}
+};
